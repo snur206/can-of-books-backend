4731fd07a695f000edb98fbd24a9702bca0f689b
SHARMARKE NUR <Snur206@yahoo.com>
2022-12-12 21:02:51 -0800

    updated

--- server.js
+++ server.js
@@ -7,10 +7,25 @@ const Book = require('./model/books.js');
 const app = express();
 const handleBooks = require('./handleBooks.js');
 app.use(cors());
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URL);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', _ => {
+  console.log('We\'re connected, playa!');
+});
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 
-app.get('/books', handleBooks);
+app.get('/books', async (request, response) => {
+  try {
+    let books = await Book.find()
+    console.log(books);
+    response.send(books);
+  } catch(e) {
+    console.log(e);
+  }
+});
 
 app.get('/test', (request, response) => {
 
